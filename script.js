d24b0b241682605b667924dfeb28a3842186ad30
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-08-04 22:43:30 +0100

    dark/light mode
    dark and light mode works

--- script.js
+++ script.js
@@ -2,8 +2,19 @@
 const header = document.querySelector("header");
 const menu = document.querySelector("#menu-icon");
 const navbar = document.querySelector(".navbar");
+const lightmode = document.querySelector("#lightmode");
 
 // FUNCTIONS
 menu.onclick = () => {
   navbar.classList.toggle("active");
+};
+
+lightmode.onclick = () => {
+  if (lightmode.classList.contains("fa-regular")) {
+    lightmode.classList.replace("fa-regular", "fa-solid");
+    document.body.classList.add("active");
+  } else {
+    lightmode.classList.replace("fa-solid", "fa-regular");
+    document.body.classList.remove("active");
+  }
 };
