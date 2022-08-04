477830dcd4a1727dda4b8d4b6fecb05f67ccb39e
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-08-04 21:00:56 +0100

    responsive_functionality
    Responsive functionality in progress.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hello World");
+// VARIABLES
+const header = document.querySelector("header");
+const menu = document.querySelector("#menu-icon");
+const navbar = document.querySelector(".navbar");
+
+// FUNCTIONS
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
