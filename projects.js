d6667d65ef3273a3fed89fe9a5891bc4a1fe3242
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-08-08 20:32:54 +0100

    projects from object

---
+++ projects.js
@@ -0,0 +1,26 @@
+export const projectsList = {
+  1: {
+    name: "Color flipper",
+    description: "Press the button to change the background color.",
+    thumbnail: "./img/projects_image/color_fliper_940x460.png",
+    githubLink: "#",
+    pageUrl: "#",
+    difficulty: 1,
+  },
+  2: {
+    name: "Color flipper",
+    description: "Press the button to change the background color.",
+    thumbnail: "./img/projects_image/color_fliper_940x460.png",
+    githubLink: "#",
+    pageUrl: "#",
+    difficulty: 4,
+  },
+  3: {
+    name: "Color flipper",
+    description: "Press the button to change the background color.",
+    thumbnail: "./img/projects_image/color_fliper_940x460.png",
+    githubLink: "#",
+    pageUrl: "#",
+    difficulty: 2,
+  },
+};
--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+// IMPORT
+import { projectsList } from "./projects.js";
+
 // VARIABLES
 const header = document.querySelector("header");
 const menu = document.querySelector("#menu-icon");
 const navbar = document.querySelector(".navbar");
 const lightmode = document.querySelector("#lightmode");
+const portfolioContent = document.querySelector(".portfolio-content");
 
 // FUNCTIONS
 menu.onclick = () => {
@@ -18,3 +22,51 @@ lightmode.onclick = () => {
     document.body.classList.remove("active");
   }
 };
+
+for (let i = 1; i <= 3; i++) {
+  const project = projectsList[i];
+  console.log(project);
+
+  const projectCard = document.createElement("div");
+  projectCard.classList.add("project-card");
+
+  const image = document.createElement("div");
+  image.classList.add("image");
+  projectCard.appendChild(image);
+
+  const img = document.createElement("img");
+  img.setAttribute("src", project["thumbnail"]);
+  image.appendChild(img);
+
+  const details = document.createElement("div");
+  details.classList.add("details");
+  projectCard.appendChild(details);
+
+  const center = document.createElement("div");
+  center.classList.add("center");
+  details.appendChild(center);
+
+  const h1 = document.createElement("h1");
+  h1.innerHTML = `${project.name}<br/>
+  <span>
+  Dificulty: ${'<i class="fa-solid fa-star"></i>'.repeat(project["difficulty"])}
+  </span>`;
+  center.appendChild(h1);
+
+  const description = document.createElement("p");
+  description.textContent = `${project["description"]}`;
+  center.appendChild(description);
+
+  const unorderdList = document.createElement("ul");
+  const gitHubLink = document.createElement("li");
+  gitHubLink.innerHTML = `<a href=${project["githubLink"]}><i class="fa-brands fa-github"></i></a>`;
+  const pageURL = document.createElement("li");
+  pageURL.innerHTML = `<a href=${project["pageUrl"]}><i class="fa-solid fa-arrow-right-to-bracket"></i></a>`;
+
+  unorderdList.appendChild(gitHubLink);
+  unorderdList.appendChild(pageURL);
+
+  center.appendChild(unorderdList);
+
+  portfolioContent.appendChild(projectCard);
+}
