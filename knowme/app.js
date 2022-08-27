1974b70f6bf9dc1035ba4aca069d361e1cdaef0b
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-08-27 16:32:59 +0100

    basic not working logic

--- knowme/app.js
+++ knowme/app.js
@@ -10,11 +10,36 @@ const videoList = [
   { name: "This goes here!", url: "./vid/pes/here.mp4" },
 ];
 
+const travelPictureList = [
+  {
+    place: "Paris",
+    thumbnailURL: "./img/travel/paris.jpg",
+    fullSizeURL: "./img/travel/paris.jpg",
+  },
+  {
+    place: "London",
+    thumbnailURL: "./img/travel/london.jpg",
+    fullSizeURL: "./img/travel/london.jpg",
+  },
+  {
+    place: "Snowdon",
+    thumbnailURL: "./img/travel/snowdon.jpg",
+    fullSizeURL: "./img/travel/snowdon.jpg",
+  },
+  {
+    place: "Wydmy",
+    thumbnailURL: "./img/travel/wydmy.jpg",
+    fullSizeURL: "./img/travel/wydmy.jpg",
+  },
+];
+
 // CONSTANTS
 const ulHTML = document.querySelector(".list");
 const mainVideo = document.querySelector(".main-video-container");
 let previousActiveItem;
+const travelSection = document.querySelector(".travel");
 
+// VIDEOS GALLERY LOGIC
 videoList.forEach((video) => {
   const liItem = document.createElement("li");
   liItem.classList.add("item");
@@ -47,4 +72,32 @@ videoList.forEach((video) => {
 
     console.log(previousActiveItem);
   });
+});
+
+// Pictures GALLERY LOGIC
+travelPictureList.forEach((picture) => {
+  const immageDiv = document.createElement("div");
+  immageDiv.classList.add("imageContainer");
+
+  const image = document.createElement("img");
+  image.src = picture.fullSizeURL;
+  image.alt = picture.place;
+  image.classList.add("galleryImg");
+
+  image.addEventListener("click", () => {
+    //popup stuff
+    backButton.style.visibility = "hidden";
+    popup.style.transform = "translateY(0)";
+    selectedImage.src = picture.fullSizeURL;
+  });
+
+  immageDiv.appendChild(image);
+  gallery.appendChild(immageDiv);
+});
+
+popup.addEventListener("click", () => {
+  backButton.style.visibility = "";
+  popup.style.transform = "translate(-100%)";
+  popup.src = "";
+  popup.alt = "";
 });
