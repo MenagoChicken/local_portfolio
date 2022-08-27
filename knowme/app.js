f9af5e1c53383e6d04071a2dbb8bb153bef34c4b
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-08-27 13:42:12 +0100

    beginning of JS

--- knowme/app.js
+++ knowme/app.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+// IMPORT DIDN'T WORK SO I JUST CREATED LIST HERE
+const videoList = [
+  { name: "Kick it!!!", url: "./vid/pes/kick it.mp4" },
+  { name: "Free kick", url: "./vid/pes/free kick.mp4" },
+  { name: "Ladies and Gentlemens Luka Modrić!", url: "./vid/pes/modric.mp4" },
+  {
+    name: "Poor Lewandowski <sad face>",
+    url: "./vid/pes/poor lewandowski.mp4",
+  },
+  { name: "This goes here!", url: "./vid/pes/this goes here.mp4" },
+];
+
+// CONSTANTS
+const ulHTML = document.querySelector(".list");
+
+console.log(ulHTML);
