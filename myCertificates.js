81f2fd58c6ae45e0d55e2cb4922a360cf9c4031f
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2023-06-19 09:33:15 +0200

    Create myCertificates.js
    new list

---
+++ myCertificates.js
@@ -0,0 +1,38 @@
+export const certificatesList = [
+	{
+		year: 2023,
+		name: "ServiceNow Certified System Administrator",
+		organizer: "ServiceNow",
+		link: "https://drive.google.com/file/d/15wDYBqs6nIQrDz5ItfHZdAP-CTdU9Km7/view?usp=sharing",
+	},
+	{
+		year: 2023,
+		name: "Micro-Certification - Performance Analytics",
+		organizer: "nowlearning",
+		link: "https://drive.google.com/file/d/167_R3f4qmKhy6ny9yFuph7jnQ1Is3bMA/view?usp=sharing",
+	},
+	{
+		year: 2023,
+		name: "Micro-Certification - Flow Designer",
+		organizer: "nowlearning",
+		link: "https://drive.google.com/file/d/16-3Otb5s_wOzS6gGoPaJdCHBsSVNNJ0Q/view?usp=sharing",
+	},
+	{
+		year: 2023,
+		name: "Micro-Certification - Welcome to ServiceNow",
+		organizer: "nowlearning",
+		link: "https://drive.google.com/file/d/15zb5Up_4P4Kuy4y0ptkwRmIxF4Mh6F7N/view?usp=sharing",
+	},
+	{
+		year: 2018,
+		name: "Java od podstaw",
+		organizer: "Software Development Academy",
+		link: "https://drive.google.com/file/d/1thVPUolcB0G2itdh5zlJDriu-NGImmu7/view?usp=sharing",
+	},
+	{
+		year: 2009,
+		name: "ECDL - Core",
+		organizer: "mcsk",
+		link: "https://drive.google.com/file/d/1tj3G-ZuhtX7GA_9JWTM3CL6_n_bvjUdX/view?usp=sharing",
+	},
+];
